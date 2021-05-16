0979e4e203ac9d243eb1c19072e96c36ea01ef66
Pablo Porzio <pabloporzio97@gmail.com>
2021-05-16 13:58:31 -0300

    Reviewed battery code

--- battery-daemon.py
+++ battery-daemon.py
@@ -1,15 +1,32 @@
 #!/usr/bin/env python
  
 import sys, time
+import dbus
 from daemon import Daemon
  
 class BatteryDaemon(Daemon):
+ """ Daemon that checks battery percentage and state using info
+ provided by `D-BUS`, and logs when the battery is full and connected to AC. """
+
+ def checkBattery(self):
+  # connects to system bus daemon       
+  bus = dbus.SystemBus()
+
+  # gets proxy object that connects to app providing battery info through an interface
+  batteryObject = bus.get_object('org.freedesktop.UPower', '/org/freedesktop/UPower/devices/battery_BAT0')
+  battery = dbus.Interface(batteryObject, 'org.freedesktop.DBus.Properties')
+
+  # gets battery percentage and state
+  batteryPercentage = battery.Get("org.freedesktop.UPower.Device", "Percentage")
+  batteryState = battery.Get("org.freedesktop.UPower.Device", "State")
+
  def run(self):
   while True:
+   self.checkBattery()
    time.sleep(1)
- 
-if __name__ == "__main__":
- daemon = BatteryDaemon('/tmp/daemon-example.pid')
+
+def main():
+ daemon = BatteryDaemon('/tmp/battery-daemon.pid')
  if len(sys.argv) == 2:
   if 'start' == sys.argv[1]:
    daemon.start()
@@ -21,6 +38,3 @@ if __name__ == "__main__":
    print("Unknown command")
    sys.exit(2)
  sys.exit(0)
-else:
- print("usage: %s start|stop|restart" % sys.argv[0])
- sys.exit(2)
